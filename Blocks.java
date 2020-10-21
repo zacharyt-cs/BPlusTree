ffa521dd765d9ec05212cc0d8c4109f654124477
Zachary <zacharytan.cs@gmail.com>
2020-10-21 12:07:27 +0800

    renamed blo to block_list; add header to block

--- Blocks.java
+++ Blocks.java
@@ -1,8 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 
+//MORE CHANGES REQUIRED
 public class Blocks {
+	// header = 10 bytes
+	public int header = 10;
 	public List<Records> recordlist= new ArrayList<Records>();
+	
 	public void addRecord(Records data){
 		recordlist.add(data);
 	}
--- Main.java
+++ Main.java
@@ -6,9 +6,6 @@ public class Main {
 
     public static int counter=0;
     static Scanner input = new Scanner(System.in);
-    
-    
-
     public static void main(String[] args) {
     
         // Main Menu
@@ -48,8 +45,8 @@ public class Main {
         int second_option;
         int n = blocksize/4;
         BPlusTree tree = new BPlusTree(n);
-        List<Blocks> blo = new ArrayList<Blocks>();
-        FileIO.readTSV("data.tsv", tree, blo);
+        List<Blocks> block_list = new ArrayList<Blocks>();
+        FileIO.readTSV("data.tsv", tree, block_list);
         
         do {
             System.out.println(
@@ -62,23 +59,21 @@ public class Main {
                 "0 to change block size");
             second_option = input.nextInt();
             switch(second_option){
+                // Incorrect
                 case 1:
-                // run Exp 1
+                    // run Exp 1: print number of blocks and size of database
                     System.out.println("--------------------Experiment 1 and 2----------------------");
-                   
 
-                    // one data block is equal to 100
-                    System.out.println("The number of blocks "+ blo.size());
-                    System.out.println("The size of database "+blo.size()*100);
-                    
+                    System.out.println("The number of blocks "+ block_list.size());
 
-                   
+                    // size of database = num of blocks * block size (WARNING: last block may not be full)
+                    System.out.println("The size of database "+block_list.size()*100);
                 break;
 
                 case 2:
-                // run Exp 2
-                System.out.println("run ex 2");
-                tree.printTree();
+                    // run Exp 2
+                    System.out.println("run ex 2");
+                    tree.printTree();
                 break;
 
                 case 3:
@@ -97,10 +92,10 @@ public class Main {
     			}
 //    			Print out the number of blocks and the block data
     			int blockCounter=0;
-    			for(int j=0; j<blo.size();j++)
+    			for(int j=0; j<block_list.size();j++)
     			{
     				//put the block into the dataBlo arraylist
-    				List<Records> dataBlo =	blo.get(j).recordlist;
+    				List<Records> dataBlo =	block_list.get(j).recordlist;
     				
     				for(int a=0;a<dataBlo.size();a++)
     				{
@@ -149,9 +144,9 @@ public class Main {
                     int blockSize=0;
                     int printNum=1;
                
-                    for(int g=0; g< blo.size();g++)
+                    for(int g=0; g< block_list.size();g++)
                     {
-                    	List<Records> rangeBlo =blo.get(g).recordlist;
+                    	List<Records> rangeBlo =block_list.get(g).recordlist;
         				
         				for(int a=0;a<rangeBlo.size();a++)
         				{
